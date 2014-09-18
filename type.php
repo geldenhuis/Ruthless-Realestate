5f0bb16b4b28d75258e71670250ec3911ecefcb3
Luke <bland_man@me.com>
2014-09-18 12:37:01 +1000

    Added files for features and type.php

---
+++ type.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Standard meta kludge -->
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="description" content="FIT2076 Assignment 2">
+    <meta name="author" content="Luke Blues (25124463)">
+    <meta name="keyword" content="">
+
+    <title>Admin - Dashboard</title>
+
+    <!-- CSS Styles -->
+    <link href="./assets/css/style.css" rel="stylesheet">
+    <link href="./assets/css/colors.css" rel="stylesheet">
+    <link href="./assets/css/box.css" rel="stylesheet">
+    <link href="./assets/css/sidebar.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+</body>
+</html>
