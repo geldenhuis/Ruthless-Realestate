dcdb8634c5e1e8ded7bfe8470b578a2cc36e9ed8
Luke <bland_man@me.com>
2014-09-22 00:48:14 +1000

    Handles Authcate Login Process

---
+++ process-login.php
@@ -0,0 +1,46 @@
+<?php
+  //create MDS constants
+  define("MONASH_DIR", "ldap.monash.edu.au");
+  define("MONASH_FILTER","o=Monash University, c=au");
+
+   $LDAPconn=@ldap_connect(MONASH_DIR);
+    if($LDAPconn)
+    {
+      $LDAPsearch=@ldap_search($LDAPconn,MONASH_FILTER,
+        "uid=".$_POST["uname"]);
+
+      if($LDAPsearch)
+      {
+        $LDAPinfo =
+          @ldap_first_entry($LDAPconn,$LDAPsearch);
+        if($LDAPinfo)
+        {
+          $LDAPresult=
+            @ldap_bind($LDAPconn,
+            ldap_get_dn($LDAPconn, $LDAPinfo),
+            $_POST["pword"]);
+        }
+        else
+        {
+          $LDAPresult=0;
+        }
+      }
+      else
+      {
+        $LDAPresult=0;
+      }
+    }
+    else
+    {
+      $LDAPresult=0;
+    }
+
+    if($LDAPresult)
+    {
+      echo "Valid User";
+    }
+    else
+    {
+      echo "Invalid User";
+    }
+?>
